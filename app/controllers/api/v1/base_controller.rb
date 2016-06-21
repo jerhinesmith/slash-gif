ab50805270a702d3de394de1ed9f951d447691e5
Justin Rhinesmith <jerhinesmith@gmail.com>
2016-06-21 09:37:28 -0700

    Ignore authenticity token

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::BaseController < ApplicationController
+  skip_before_action :verify_authenticity_token
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
   private
